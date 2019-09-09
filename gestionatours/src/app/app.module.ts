dc7a8e7492b37c98a90385598fc3bfd989cbf570
marcorodriguez2013 <marcorodriguez2013@outlook.com>
2019-09-09 16:04:31 -0500

    comit
    tour

--- gestionatours/src/app/app.module.ts
+++ gestionatours/src/app/app.module.ts
@@ -11,6 +11,29 @@ import { TopdestinosComponent } from './components/topdestinos/topdestinos.compo
 import { TourslocalesComponent } from './components/tourslocales/tourslocales.component';
 import { ComentariosComponent } from './components/comentarios/comentarios.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './components/login/login.component';
+
+
+// modulos para inicio de sesion para redes sociales
+
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("466947457626-v9tdlf749rf54oiiscrqr6m2na9n82ri.apps.googleusercontent.com")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("1673917412742722")
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -22,13 +45,18 @@ import { AppRoutingModule } from './app-routing.module';
     ToptoursComponent,
     TopdestinosComponent,
     TourslocalesComponent,
-    ComentariosComponent
+    ComentariosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: AuthServiceConfig,
+    useFactory: provideConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- gestionatours/src/app/components/navbar/navbar.component.ts
+++ gestionatours/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
+
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  private user: SocialUser;
+  private loggedIn: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(this.user);
+
+      this.loggedIn = (user != null);
+    });
   }
 
 }
