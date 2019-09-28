b243b16b7a58a36a7f9670d1349d7029b6969adb
neto93 <netoticonaquispe@gmail.com>
2019-09-28 11:44:29 -0500

    eliminando

--- 28-set-backGestionaTours/Api/Routes/busqueda.ts
+++
@@ -1,5 +0,0 @@
-import {Router} from 'express'
-import { getBusqueda } from '../Controllers/Busqueda';
-
-export let BusquedaTotal_Router = Router();
-BusquedaTotal_Router.post('/busqueda',getBusqueda)
--- 28-set-backGestionaTours/Api/Routes/provincia.ts
+++
@@ -1,5 +0,0 @@
-import { Router} from 'express'
-import { getTourByProvincia} from '../Controllers/provinvia'
-
-export let Provin_Router = Router();
-Provin_Router.get('/turism_by_prov/:prov',getTourByProvincia)
