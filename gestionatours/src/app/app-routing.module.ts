23eed8fd834a15f7c5c68247e594b686d083c417
alexgincho <51126316+alexgincho@users.noreply.github.com>
2019-10-02 16:59:28 +0100

    Update app-routing.module.ts

--- gestionatours/src/app/app-routing.module.ts
+++ gestionatours/src/app/app-routing.module.ts
@@ -5,20 +5,41 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ToursComponent } from './components/tours/tours.component';
 import { TourComponent } from './components/tour/tour.component';
-import { ProductoComponent } from './components/producto/producto.component';
+import { AdminComponent } from './administrador/components/admin/admin.component';
+import { HomeAdminComponent } from './administrador/components/home-admin/home-admin.component';
 
 
 const APP_ROUTES: Routes = [
-  
+
   { path: '', redirectTo:'/home', pathMatch: 'full' },
   { path: 'tours', component: ToursComponent },
   { path: 'tour', component: TourComponent },
+
   { path: '**', component: HomeComponent },
+
+
+  
+  { path: '**', component: HomeComponent },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeAdminComponent
+    },
+    {
+        path: 'registro',
+        component: HomeAdminComponent
+    },
+        
+    ]
+}
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(APP_ROUTES)],
+  imports: [RouterModule.forRoot(APP_ROUTES/* ,{useHash:true} */)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
