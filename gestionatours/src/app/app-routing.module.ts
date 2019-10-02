c0c6a49573209f6f6d19321df3e63f7de693b069
by Wilian Llano <38366996+opencodigo@users.noreply.github.com>
2019-10-02 18:23:28 -0500

    commit front con rutas hasta factura

--- gestionatours/src/app/app-routing.module.ts
+++ gestionatours/src/app/app-routing.module.ts
@@ -5,19 +5,20 @@ import { ToursComponent } from './components/tours/tours.component';
 import { TourComponent } from './components/tour/tour.component';
 import { AdminComponent } from './administrador/components/admin/admin.component';
 import { HomeAdminComponent } from './administrador/components/home-admin/home-admin.component';
+import { PagarproductoComponent } from './components/pagarproducto/pagarproducto.component';
+import { FacturaComponent } from './components/factura/factura.component';
+import { ProductoComponent } from './components/producto/producto.component';
 
 
 const APP_ROUTES: Routes = [
 
   { path: '', redirectTo:'/home', pathMatch: 'full' },
   { path: 'tours', component: ToursComponent },
-  { path: 'tour', component: TourComponent },
-
+  { path: 'tour', component: ProductoComponent },
+  { path: 'reserva', component: PagarproductoComponent },
+  { path: 'factura', component: FacturaComponent },
   { path: '**', component: HomeComponent },
-
-
   
-  { path: '**', component: HomeComponent },
   {
     path: 'admin',
     component: AdminComponent,
