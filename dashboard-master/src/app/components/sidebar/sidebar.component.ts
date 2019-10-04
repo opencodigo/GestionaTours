998c267ecad1382c52972e441ec7c87531e32246
by Wilian Llano <38366996+opencodigo@users.noreply.github.com>
2019-10-03 20:13:04 -0500

    master limpio todos rutas y contenido

--- dashboard-master/src/app/components/sidebar/sidebar.component.ts
+++ dashboard-master/src/app/components/sidebar/sidebar.component.ts
@@ -8,14 +8,14 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+    { path: '/principal', title: 'Principal',  icon: 'dashboard', class: '' },
+    { path: '/usuario', title: 'Mant. Usuarios',  icon:'person', class: '' },
+    { path: '/producto', title: 'Mant. Producto',  icon:'content_paste', class: '' },
+    { path: '/tour', title: 'Mant. Tour',  icon:'library_books', class: '' },
+    { path: '/reserva', title: 'Mant. Reserva',  icon:'content_paste', class: '' },
+    { path: '/factura', title: 'Mant. Factura',  icon:'library_books', class: '' },
+    { path: '/programa', title: 'Mant. Programación',  icon:'notifications', class: '' },
+    { path: '/mantenimiento', title: 'REPORTAR AVERIAS',  icon:'unarchive', class: 'active-pro' },
 ];
 
 @Component({
--- dashboard-master/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ dashboard-master/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -52,12 +52,12 @@ export const AdminLayoutRoutes: Routes = [
     //         component: UpgradeComponent
     //     }]
     // }
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'table-list',     component: TableListComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent },
+    { path: 'principal',      component: DashboardComponent },
+    { path: 'usuario',   component: UserProfileComponent },
+    { path: 'producto',     component: TableListComponent },
+    { path: 'tour',     component: TypographyComponent },
+    { path: 'reserva',          component: IconsComponent },
+    { path: 'factura',           component: MapsComponent },
+    { path: 'programa',  component: NotificationsComponent },
+    { path: 'mantenimiento',        component: UpgradeComponent },
 ];
